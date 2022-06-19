e4001bca9c3478417955da093534a46878bb8570
Jose Rodolfo <joserodolforodrigues2021@gmail.com>
2022-06-19 15:49:39 -0300

    first commit

---
+++ src/test/java/com/univas/JoseRodolfo/JoseRodolfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.univas.JoseRodolfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoseRodolfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
